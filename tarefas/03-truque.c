6dd77788e7ec300477afc9b843a12020439a65a5
Cakios <caio_henrique2006@outlook.com>
2026-08-29 12:05:10 -0300

    ,

--- tarefas/03-truque.c
+++ tarefas/03-truque.c
@@ -9,31 +9,53 @@ void input_v(int n, int V[])
     }
 }
 
-void build_r(int r, int n, int V[], int R[])
+void build_r(int tam_r, int r, int n, int V[], int R[])
 {
-    // printf("Valores: %d %d", r, n);
-    printf("Tamanho de R: %f", ceil(n / r));
-    for (int i = 0; i < ceil(n / r); i++)
+    for (int i = 0; i < tam_r; i++)
     {
         int soma = 0;
-        for (int j = 0; j < r; j++)
+        for (int j = i * r; j < ((i + 1) * r); j++)
         {
-            soma += V[(i * r) + j];
+            if (j < n)
+            {
+                soma += V[j];
+            }
+            else
+            {
+                break;
+            }
         }
         R[i] = soma;
-        printf("Somas: %d \n", soma);
     }
 }
 
-/*
-int operSoma(int V[], int R[], int i, int j)
+void operSoma(int V[], int r, int R[], int a, int b)
 {
+    int i_atual = a;
+    int soma = 0;
+    while ((i_atual) % r != 0 && i_atual <= b)
+    {
+        soma += V[i_atual];
+        i_atual += 1;
+    }
+    while (b - (i_atual) >= r)
+    {
+        soma += R[(i_atual) / r];
+        i_atual += r;
+    }
+    while ((i_atual - 1) != b)
+    {
+        soma += V[i_atual];
+        i_atual += 1;
+    }
+    printf("%d \n", soma);
 }
 
-int operSubst(int V[], int R[], int i, int x)
+void operSubst(int V[], int r, int R[], int a, int b)
 {
+    R[(a / r)] = R[(a / r)] + (b - V[a]);
+    V[a] = b;
 }
-*/
 
 int main()
 {
@@ -41,31 +63,24 @@ int main()
     scanf("%d", &n);
     int V[n];
     input_v(n, V);
-    for (int i = 0; i < n; i++)
-    {
-        printf("%d ", V[i]);
-    }
-    printf("\n");
     int r = ceil(sqrt(n));
-    printf("r = %d \n", r);
-    int tam_r = ceil(r / n);
+    int tam_r = ceil((double)n / r);
     int R[tam_r];
-    build_r(r, n, V, R);
-    /*
-    for (int i = 0; i < r; i++)
+    build_r(tam_r, r, n, V, R);
+
+    char mod;
+    int a, b;
+    while (scanf(" %c %d %d", &mod, &a, &b))
     {
-        printf("%d \n", R[i]);
-    }
-    /*
-    while (1)
-    {
-        scanf("%d", &N);
-        if (N == 0)
+        if (mod == 's')
         {
-            break;
+            operSoma(V, tam_r, R, a, b);
+        }
+        if (mod == 'a')
+        {
+            operSubst(V, tam_r, R, a, b);
         }
     }
-    */
 
     return 0;
 }
